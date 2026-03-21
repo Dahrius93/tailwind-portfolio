0af29a81c8995919fe586437591757ad166bde41
Dahrius93 <dariolauretta93@gmail.com>
2026-03-21 11:58:31 +0100

    Skills & About

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Skills from "./components/Skills";
+import About from "./components/About";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Skills />
+      <About />
     </>
   );
 };
